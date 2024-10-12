41c97be54e30624a2b089f448b572aa9c58d5171
sami <ahmedhossain.buet@gmail.com>
2024-10-12 17:07:26 +0600

    fixed tinacms workflow for github pages

--- tina/config.ts
+++ tina/config.ts
@@ -11,9 +11,9 @@ export default defineConfig({
   branch,
 
   // Get this from tina.io
-  clientId: process.env.TINA_PUBLIC_CLIENT_ID,
+  clientId: "dc4f2f3b-ea7c-4087-b9e3-12003454d61d",
   // Get this from tina.io
-  token: process.env.TINA_TOKEN,
+  token: "ba6f05b8477251b582c45f44e67748b7391b43c0",
 
   build: {
     outputFolder: "admin",
